60d54f9fcdbe98357bac64cc308c3312fe865ea6
cheesits456 <quin@cheesits456.dev>
2020-12-13 20:57:09 -0800

    Update index.js

--- index.js
+++ index.js
@@ -8,28 +8,16 @@ module.exports = text => {
             "the", "and", "ing", "ent", "ion", "her", "for", "tha", "nth", "int", "yeet", "lol", "ey", "ck"
         ],
         es: [
-            ' que ', ' de ', ' no ', ' a ', ' la ', ' el ', ' es ', ' y ', ' en ', ' lo ',
-            ' un ', ' por ', ' me ', ' una ', ' te ', ' los ', ' se ', ' con ', ' para ', ' mi ',
-            ' esta ', ' si ', ' bien ', ' pero ', ' yo ', ' eso ', ' las ', ' si ', ' su ', ' tu ',
-            'del', 'que', 'ent', 'ion', 'ela', 'con', 'sde', 'ade', 'cio', 'nte'
+            " que ", " de ", " no ", " a ", " la ", " el ", " es ", " y ", " en ", " lo ",
+            " un ", " por ", " me ", " una ", " te ", " los ", " se ", " con ", " para ", " mi ",
+            " esta ", " si ", " bien ", " pero ", " yo ", " eso ", " las ", " si ", " su ", " tu ",
+            "del", "que", "ent", "ion", "ela", "con", "sde", "ade", "cio", "nte"
         ],
         fr: [
-            ' je ', ' de ', ' est ', ' pas ', ' le ', ' vous ', ' la ', ' tu ', ' que ', ' un ',
-            ' il ', ' et ', ' a ', ' ne ', ' les ', ' ce ', ' en ', ' on ', ' ca ', ' une ',
-            ' ai ', ' pour ', ' des ', ' moi ', ' qui ', ' nous ', ' y ', ' mais ', ' me ', ' dans ',
-            'ent', 'les', 'ion', 'des', 'ede', 'que', 'est', 'tio', 'ant', 'par'
-        ],
-        pt: [
-            ' que ', ' nao ', ' de ', ' um ', ' para ', ' eu ', ' se ', ' me ', ' uma ', ' esta ',
-            ' com ', ' do ', ' por ', ' te ', ' os ', ' bem ', ' em ', ' ele ', ' isso ', ' mas ',
-            ' da ', ' como ', ' no ', ' sim ', ' as ', ' mais ', ' na ', ' meu ', ' você ', ' aqui ',
-            'que', 'ent', 'com', 'nte', 'est', 'ava', 'ndo', 'ado', 'ara', 'par'
-        ],
-        tr: [
-            ' ama', ' demek ','çok' 'ara ', ' tr','sa ', ' as ', ' ne ', ' garip ',
-            ' kopya ', ' yap ', ' bb ', ' ana ', ' sayfa ', ' Çerez ', ' Dil ', ' İyi ', ' Sorun ', ' Menü',
-            ' HAYIR ', 'kuş','bilgisayar''su'
-            'evet', 'Uygulama','et', 'O' 'sen' 'ben' 'kız' 'erkek' 'onun' 'için' 'biz' 'niye' 'onlar' 'yine'
+            " je ", " de ", " est ", " pas ", " le ", " vous ", " la ", " tu ", " que ", " un ",
+            " il ", " et ", " a ", " ne ", " les ", " ce ", " en ", " on ", " ca ", " une ",
+            " ai ", " pour ", " des ", " moi ", " qui ", " nous ", " y ", " mais ", " me ", " dans ",
+            "ent", "les", "ion", "des", "ede", "que", "est", "tio", "ant", "par"
         ],
         pl: [
             " ty ", " ja ", " do ", " i ", " że ", " to ", " z ", " ja ",
@@ -38,6 +26,18 @@ module.exports = text => {
             " jej ", " jemu ", " do ", " kurwa ", " debil ", " chuj ", " przez ", " od ", " po ", "że",
             " jeszcze ", " tej ", " może ", " jednak ", " bardzo ", " tysięcy ", " siema ", " innymi ",
             " m.in. ", " około "
+        ],
+        pt: [
+            " que ", " nao ", " de ", " um ", " para ", " eu ", " se ", " me ", " uma ", " esta ",
+            " com ", " do ", " por ", " te ", " os ", " bem ", " em ", " ele ", " isso ", " mas ",
+            " da ", " como ", " no ", " sim ", " as ", " mais ", " na ", " meu ", " você ", " aqui ",
+            "que", "ent", "com", "nte", "est", "ava", "ndo", "ado", "ara", "par"
+        ],
+        tr: [
+            " ama", " demek ", "çok", "ara ", " tr", "sa ", " as ", " ne ", " garip ",
+            " kopya ", " yap ", " bb ", " ana ", " sayfa ", " Çerez ", " Dil ", " İyi ", " Sorun ", " Menü",
+            " HAYIR ", "kuş", "bilgisayar", "su",
+            "evet", "Uygulama", "et", "O", "sen", "ben", "kız", "erkek", "onun", "için", "biz", "niye", "onlar", "yine"
         ]
     };
 
